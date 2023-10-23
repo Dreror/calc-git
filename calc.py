f8000dbdc19b89a2025d00b4b17502994c1c95ed
Dreror <lagutams@gmail.com>
2023-10-23 17:28:25 +0300

    add function for calc

---
+++ calc.py
@@ -0,0 +1,27 @@
+def add(x, y):
+    return x + y
+
+
+def dis(x, y):
+    return x - y
+
+
+def mul(x, y):
+    return x * y
+
+
+def dell(x, y):
+    return x / y
+
+
+def sqr(x, y):
+    return x ** y
+
+
+def qua(x):
+    return x ** 2
+
+
+def qb(x):
+    return x ** 3
+
